30ca081a790290a37cecc3347263c082926b0019
kusupon <kusupon0628@gmail.com>
2022-03-31 12:53:40 +0900

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,13 +20,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'kusuponのブログです！！' },
       { name: 'format-detection', content: 'telephone=no' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: 'kusuponのブログです'
-      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
